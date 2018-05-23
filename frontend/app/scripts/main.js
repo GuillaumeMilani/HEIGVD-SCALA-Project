7a1cbd69d56e9cc47a39b46b5c8d481e34118c0b
Edward Ransome <edward.ransome@heig-vd.ch>
2018-05-23 16:30:18 +0200

    quick update

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -2,5 +2,15 @@
 document.getElementById('getNewPuzzle').addEventListener('click', getNewPuzzle);
 
 function getNewPuzzle() {
+    var x = document.getElementById('boxWidth').value;
+    var y = document.getElementById('boxHeight').value;
+    console.log(x)
+    console.log(y)
+    
+    if(x < 0 || y < 0 || y > 20 || y > 20){
+        alert('Please enter valid dimensions for puzzle');
+        return
+    }
+    document.getElementById('getNewPuzzle');
     console.log('Not implemented');
 }
