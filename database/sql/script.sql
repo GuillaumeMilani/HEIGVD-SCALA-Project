74d10ace00f3cae615a6157bd41cf5a002389118
Guillaume Milani <milani.guillaume@gmail.com>
2018-06-16 16:49:20 +0200

    DB script cleanup

--- database/sql/script.sql
+++ database/sql/script.sql
@@ -5,19 +5,16 @@
 
 DROP TABLE IF EXISTS image_has_label;
 DROP TABLE IF EXISTS image;
-DROP TABLE IF EXISTS LABEL;
-DROP TABLE IF EXISTS USER;
-DROP TABLE IF EXISTS LEVEL;
+DROP TABLE IF EXISTS label;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS level;
 
 -- -----------------------------------------------------
 -- Table  label
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS LABEL (
-  id    INT NOT NULL AUTO_INCREMENT,
-  LABEL VARCHAR(45
-        )   NOT NULL,
-  PRIMARY KEY (id
-  )
+CREATE TABLE IF NOT EXISTS label (
+  id    INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  LABEL VARCHAR(45) NOT NULL
 )
   ENGINE = InnoDB;
 
@@ -25,21 +22,10 @@ CREATE TABLE IF NOT EXISTS LABEL (
 -- Table  image
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS image (
-  id       INT NOT NULL AUTO_INCREMENT,
-  filename VARCHAR(45
-           )   NOT NULL,
-  label_id INT NULL,
-  PRIMARY KEY (id
-  ),
-  INDEX    FK_IMAGE_1_IDX(label_id ASC
-           ),
-  CONSTRAINT fk_image_1
-  FOREIGN KEY (label_id
-  )
-  REFERENCES LABEL (id
-  )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
+  id       INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  filename VARCHAR(45) NOT NULL,
+  label_id INT         NULL,
+  FOREIGN KEY (label_id) REFERENCES label (id)
 )
   ENGINE = InnoDB;
 
@@ -47,74 +33,35 @@ CREATE TABLE IF NOT EXISTS image (
 -- Table  image_has_label
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS image_has_label (
-  label_id INT        NOT NULL,
-  image_id INT        NOT NULL,
-  clicks   MEDIUMTEXT NOT NULL,
-  PRIMARY KEY (label_id, image_id
-  ),
-  INDEX    FK_LABEL_HAS_IMAGE_IMAGE1_IDX(image_id ASC),
-  INDEX    FK_LABEL_HAS_IMAGE_LABEL1_IDX(label_id ASC),
-  CONSTRAINT fk_label_has_image_label1
-  FOREIGN KEY (label_id
-  )
-  REFERENCES LABEL (id
-  )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-  CONSTRAINT fk_label_has_image_image1
-  FOREIGN KEY (image_id
-  )
-  REFERENCES image (id
-  )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
+  label_id INT NOT NULL,
+  image_id INT NOT NULL,
+  clicks   INT NOT NULL,
+  PRIMARY KEY (label_id, image_id),
+  FOREIGN KEY (label_id) REFERENCES label (id),
+  FOREIGN KEY (image_id) REFERENCES image (id)
 )
   ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table  level
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS LEVEL (
-  id         INT NOT NULL AUTO_INCREMENT,
-  NAME       VARCHAR(45
-             )   NULL,
-  next_level INT NOT NULL,
-  PRIMARY KEY (id
-  ),
-  INDEX      FK_LEVEL_LEVEL1_IDX(next_level ASC
-             ),
-  CONSTRAINT fk_level_level1
-  FOREIGN KEY (next_level
-  )
-  REFERENCES LEVEL (id
-  )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS level (
+  id         INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  NAME       VARCHAR(45) NULL,
+  next_level INT         NOT NULL,
+  FOREIGN KEY (next_level) REFERENCES level (id)
 )
   ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table  user
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS USER (
-  id       INT NOT NULL AUTO_INCREMENT,
-  LOGIN    VARCHAR(45
-           )   NULL,
-  PASSWORD VARCHAR(45
-           )   NULL,
-  score    VARCHAR(45
-           )   NULL,
-  level_id INT NOT NULL,
-  PRIMARY KEY (id, level_id
-  ),
-  INDEX    FK_USER_LEVEL1_IDX(level_id ASC
-           ),
-  CONSTRAINT fk_user_level1
-  FOREIGN KEY (level_id
-  )
-  REFERENCES LEVEL (id
-  )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS user (
+  id       INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  LOGIN    VARCHAR(45) NULL,
+  PASSWORD VARCHAR(45) NULL,
+  score    VARCHAR(45) NULL,
+  level_id INT         NOT NULL,
+  FOREIGN KEY (level_id) REFERENCES level (id)
 )
   ENGINE = InnoDB;
